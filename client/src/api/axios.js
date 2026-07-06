f827668ac479d547cd149d2eb600181165af40a9
Praveenkumar Teli <praveenteli1810@gmail.com>
2026-07-06 21:16:00 +0530

    Update API base URL to deployed backend

--- client/src/api/axios.js
+++ client/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://hyperlocal-marketplace-api.onrender.com/api",
 });
 
 // Automatically attach the JWT token to every request, if it exists
